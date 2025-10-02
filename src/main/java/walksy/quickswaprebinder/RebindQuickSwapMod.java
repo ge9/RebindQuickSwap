5e068cd7598b7e7e6bcf2ac9c795b9276558776d
g <kensho.tsurusaki@gmail.com>
2025-10-02 17:57:48 +0900

    add alternative key

--- src/main/java/walksy/quickswaprebinder/RebindQuickSwapMod.java
+++ src/main/java/walksy/quickswaprebinder/RebindQuickSwapMod.java
@@ -12,6 +12,8 @@ public class RebindQuickSwapMod implements ModInitializer {
 
     public static KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(
             new KeyBinding("Quick Swap Keybind", GLFW.GLFW_KEY_LEFT_SHIFT, "Walksy's Quick Swap Rebind"));
+    public static KeyBinding keyBinding2 = KeyBindingHelper.registerKeyBinding(
+            new KeyBinding("Quick Swap Keybind 2", GLFW.GLFW_KEY_LEFT_SHIFT, "Walksy's Quick Swap Rebind"));
 
     @Override
     public void onInitialize() {
@@ -24,8 +26,12 @@ public class RebindQuickSwapMod implements ModInitializer {
         boolean bl2 = ((KeybindingAccessor)keyBinding).getKey().getCategory() == InputUtil.Type.MOUSE;
         boolean rtrn = (bl2 ? isMouseButtonPressed(MinecraftClient.getInstance().getWindow().getHandle(), code)
                 : isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), code));
+        int code2 = InputUtil.fromTranslationKey(keyBinding2.getBoundKeyTranslationKey()).getCode();
+        boolean bl22 = ((KeybindingAccessor)keyBinding2).getKey().getCategory() == InputUtil.Type.MOUSE;
+        boolean rtrn2 = (bl22 ? isMouseButtonPressed(MinecraftClient.getInstance().getWindow().getHandle(), code2)
+                : isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), code2));
 
-        return rtrn;
+        return rtrn || rtrn2;
     }
 
     private static boolean isKeyPressed(long handle, int code) {
