844664b71f0878ad1de1dfc00285811af2234ccf
g <kensho.tsurusaki@gmail.com>
2026-03-26 17:55:18 +0900

    update 26.1

--- src/main/java/walksy/quickswaprebinder/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/walksy/quickswaprebinder/mixin/AbstractContainerScreenMixin.java
@@ -1,26 +1,11 @@
 package walksy.quickswaprebinder.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.MouseHandler;
 import net.minecraft.client.input.MouseButtonEvent;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.inventory.ClickType;
-import net.minecraft.network.chat.Component;
-import org.jetbrains.annotations.Nullable;
-import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import walksy.quickswaprebinder.RebindQuickSwapMod;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import com.mojang.blaze3d.platform.InputConstants;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
-
 
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreenMixin {
